833ec52fa2164e642e189188fb3db3d1c0a8dde3
Shihara-Dilshan <shiharadotadilshanid@gmail.com>
2021-07-21 08:38:32 +0530

    file systme module added

--- app/sinhalaocr/components/Header.js
+++ app/sinhalaocr/components/Header.js
@@ -1,26 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {
   StyleSheet,
   View,
   TouchableOpacity,
   Dimensions
 } from 'react-native';
+import { useDimensions, useDeviceOrientation } from '@react-native-community/hooks'
 import Icon from 'react-native-vector-icons/Ionicons';
+import RNFetchBlob from 'rn-fetch-blob';
 
 export default Header = ({navigation}) => {
+
+    RNFetchBlob.fs.ls(RNFetchBlob.fs.dirs.SDCardDir+'/Pictures/RNSketchCanvas/').then(files => {
+      console.log(files);
+    }).catch(error => console.log(error))
+ 
     return(
-        <View style={styles.header}>
+        <View style={{
+          height: 60, 
+          padding: 15, 
+          backgroundColor: 'darkslateblue', 
+          width: useDimensions().screen.width,
+          flexDirection: 'row'}}>
           <TouchableOpacity onPress={() => {navigation.toggleDrawer()}}><Icon name="menu-outline" size={30} color="white" /></TouchableOpacity>
         </View>
     );
 }
-
-const styles = StyleSheet.create({
-    header: {
-      height: 60, padding: 15, 
-      backgroundColor: 'darkslateblue', 
-      width: Dimensions.get('window').width, 
-      flexDirection: 'row'
-    }
-  });
-   
--- app/sinhalaocr/screens/NotificationsScreen.js
+++ app/sinhalaocr/screens/NotificationsScreen.js
@@ -1,24 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {
   View,
-  StyleSheet
+  Text,
+  FlatList
 } from 'react-native';
 import Header from '../components/Header';
 
 export default function NotificationsScreen({ navigation }) {
+    const [sss, setSss] = useState([1,3,2,43,11,12,123,13,45,66,88,99,33,344343,3443434232,233232,2323]);
     return (
         <View style={{flex:1}}>
             <Header navigation={navigation}/>
-            <View style={style.container}>
+            <View style={{flex: 1}}>
+                <FlatList 
+                    data={sss}
+                    keyExtractor={ (s) => s}
+                    renderItem={(s) => {
+                        return(
+                            <Text onPress={() => alert(2)}>dffdf</Text>
+                        );
+                    }}
+                />
             </View>
         </View>
     );
 }
 
-const style = StyleSheet.create({
-    container: {
-        flex: 1, 
-        alignItems: 'center', 
-        justifyContent: 'center'
-    }       
-});
